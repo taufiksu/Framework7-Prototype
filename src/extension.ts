dcc648a5b8ea12b6c101ae304f5a4810c6649824
Taufik Sulaeman P <taufiksu@gmail.com>
2022-04-05 15:55:56 +0700

    Prepare for macos and linux

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ export function activate(context: vscode.ExtensionContext) {
 
       // Donwload package.json
       terminal.sendText(
-        'wget https://gist.github.com/taufiksu/487d4ea119a53bbbf7572ebdc440938d/raw/c70ae88cec454ed377fc86252bbb7ff1ed8f801f/package.json -OutFile package.json }"',
+        'powershell -command "& { iwr https://gist.github.com/taufiksu/487d4ea119a53bbbf7572ebdc440938d/raw/c70ae88cec454ed377fc86252bbb7ff1ed8f801f/package.json -OutFile package.json }"',
         true
       );
 
@@ -31,11 +31,11 @@ export function activate(context: vscode.ExtensionContext) {
       // Download Sample CSS
       terminal.sendText("cd css", true);
       terminal.sendText(
-        'wget https://gist.github.com/taufiksu/4f99dad2448cd00a03a98fac0c0b2e04/raw/a9849bbad89ce1e9210ddf34c1c81219611de718/app.css -OutFile app.css }"',
+        'powershell -command "& { iwr https://gist.github.com/taufiksu/4f99dad2448cd00a03a98fac0c0b2e04/raw/a9849bbad89ce1e9210ddf34c1c81219611de718/app.css -OutFile app.css }"',
         true
       );
       terminal.sendText(
-        'wget https://gist.github.com/taufiksu/6daddbb7403ed8e4701180dcd2c4ea94/raw/16fdcf61a2a4aa1c64d7a87f3d2726ab929bd6d1/icons.css -OutFile icons.css }"',
+        'powershell -command "& { iwr https://gist.github.com/taufiksu/6daddbb7403ed8e4701180dcd2c4ea94/raw/16fdcf61a2a4aa1c64d7a87f3d2726ab929bd6d1/icons.css -OutFile icons.css }"',
         true
       );
       terminal.sendText("cd ..", true);
@@ -43,11 +43,11 @@ export function activate(context: vscode.ExtensionContext) {
       // Download Sample JS
       terminal.sendText("cd js", true);
       terminal.sendText(
-        'wget https://gist.github.com/taufiksu/722fb89d0425511473c556e4871d6f5f/raw/66388dfd82a2977e5e642a5ce946da3a654f3ae4/routes.js -OutFile routes.js }"',
+        'powershell -command "& { iwr https://gist.github.com/taufiksu/722fb89d0425511473c556e4871d6f5f/raw/66388dfd82a2977e5e642a5ce946da3a654f3ae4/routes.js -OutFile routes.js }"',
         true
       );
       terminal.sendText(
-        'wget https://gist.github.com/taufiksu/d387003d6666fa5f45784e23d85a1cc2/raw/81ea9f2d91e0899a5ad0775051b8d2924f237656/app.js -OutFile app.js }"',
+        'powershell -command "& { iwr https://gist.github.com/taufiksu/d387003d6666fa5f45784e23d85a1cc2/raw/81ea9f2d91e0899a5ad0775051b8d2924f237656/app.js -OutFile app.js }"',
         true
       );
       terminal.sendText("cd ..", true);
@@ -55,18 +55,18 @@ export function activate(context: vscode.ExtensionContext) {
       // Download Sample Blank Page
       terminal.sendText("cd pages", true);
       terminal.sendText(
-        'wget https://gist.github.com/taufiksu/570b2058173bfa6c1546df9094364726/raw/9245cf418849d9b11f5ccfeedd03b3e195104fb8/home.html -OutFile home.html }"',
+        'powershell -command "& { iwr https://gist.github.com/taufiksu/570b2058173bfa6c1546df9094364726/raw/9245cf418849d9b11f5ccfeedd03b3e195104fb8/home.html -OutFile home.html }"',
         true
       );
       terminal.sendText(
-        'wget https://gist.github.com/taufiksu/51b474cbafe41ee6cfb2531852018a1c/raw/987d5e0a28216201967197d5792b1fcc27248ac3/404.html -OutFile 404.html }"',
+        'powershell -command "& { iwr https://gist.github.com/taufiksu/51b474cbafe41ee6cfb2531852018a1c/raw/987d5e0a28216201967197d5792b1fcc27248ac3/404.html -OutFile 404.html }"',
         true
       );
       terminal.sendText("cd ..", true);
 
       // Download Index and Open
       terminal.sendText(
-        'wget https://gist.github.com/taufiksu/ebef8e3a137e003db3fcb44c92c8254b/raw/8b17d094e0bf884e69470a481ec1bd138fd7b3b3/index.html -OutFile index.html }"',
+        'powershell -command "& { iwr https://gist.github.com/taufiksu/ebef8e3a137e003db3fcb44c92c8254b/raw/8b17d094e0bf884e69470a481ec1bd138fd7b3b3/index.html -OutFile index.html }"',
         true
       );
       terminal.sendText("code index.html", true);
@@ -142,6 +142,81 @@ export function activate(context: vscode.ExtensionContext) {
       // Download Index and Open
       terminal.sendText(
         'wget https://gist.github.com/taufiksu/ebef8e3a137e003db3fcb44c92c8254b/raw/8b17d094e0bf884e69470a481ec1bd138fd7b3b3/index.html',
+        true
+      );
+      terminal.sendText("code index.html", true);
+
+      // Close terminal
+      terminal.sendText("exit", true);
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("f7.BlankPageSingleMac", () => {
+      const terminal = vscode.window.createTerminal(
+        "F7 Installer",
+        process.env.COMSPEC
+      );
+      terminal.show(true);
+
+      // Donwload package.json
+      terminal.sendText(
+        'curl https://gist.github.com/taufiksu/487d4ea119a53bbbf7572ebdc440938d/raw/c70ae88cec454ed377fc86252bbb7ff1ed8f801f/package.json -o package.json',
+        true
+      );
+
+      // Get Framework7 Resouces
+      terminal.sendText("npm install", true);
+
+      // Clean
+      terminal.sendText("rm package.json", true);
+      terminal.sendText("rm package-lock.json", true);
+      terminal.sendText("rm -r node_modules", true);
+
+      terminal.sendText("cd www", true);
+      terminal.sendText("mkdir css", true);
+      terminal.sendText("mkdir js", true);
+      terminal.sendText("mkdir pages", true);
+
+      // Download Sample CSS
+      terminal.sendText("cd css", true);
+      terminal.sendText(
+        'curl https://gist.github.com/taufiksu/4f99dad2448cd00a03a98fac0c0b2e04/raw/a9849bbad89ce1e9210ddf34c1c81219611de718/app.css -o app.css',
+        true
+      );
+      terminal.sendText(
+        'curl https://gist.github.com/taufiksu/6daddbb7403ed8e4701180dcd2c4ea94/raw/16fdcf61a2a4aa1c64d7a87f3d2726ab929bd6d1/icons.css -o icons.css',
+        true
+      );
+      terminal.sendText("cd ..", true);
+
+      // Download Sample JS
+      terminal.sendText("cd js", true);
+      terminal.sendText(
+        'curl https://gist.github.com/taufiksu/722fb89d0425511473c556e4871d6f5f/raw/66388dfd82a2977e5e642a5ce946da3a654f3ae4/routes.js -o routes.js',
+        true
+      );
+      terminal.sendText(
+        'curl https://gist.github.com/taufiksu/d387003d6666fa5f45784e23d85a1cc2/raw/81ea9f2d91e0899a5ad0775051b8d2924f237656/app.js -o app.js',
+        true
+      );
+      terminal.sendText("cd ..", true);
+
+      // Download Sample Blank Page
+      terminal.sendText("cd pages", true);
+      terminal.sendText(
+        'curl https://gist.github.com/taufiksu/570b2058173bfa6c1546df9094364726/raw/9245cf418849d9b11f5ccfeedd03b3e195104fb8/home.html -o home.html',
+        true
+      );
+      terminal.sendText(
+        'curl https://gist.github.com/taufiksu/51b474cbafe41ee6cfb2531852018a1c/raw/987d5e0a28216201967197d5792b1fcc27248ac3/404.html -o 404.html',
+        true
+      );
+      terminal.sendText("cd ..", true);
+
+      // Download Index and Open
+      terminal.sendText(
+        'curl https://gist.github.com/taufiksu/ebef8e3a137e003db3fcb44c92c8254b/raw/8b17d094e0bf884e69470a481ec1bd138fd7b3b3/index.html -o index.html',
         true
       );
       terminal.sendText("code index.html", true);
